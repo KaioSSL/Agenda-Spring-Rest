6cf9e950da80cc1a0b4ff6a2327afa4101c27d64
kaio.barbosa <kaiofabiogs06@gmail.com>
2021-06-18 17:34:13 -0300

    API Agenda RestFull Base

---
+++ src/main/java/br/com/agenda/AgendaRestFull/AgendaRestFullApplication.java
@@ -0,0 +1,13 @@
+package br.com.agenda.AgendaRestFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaRestFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agenda/AgendaRestFull/AgendaRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agenda.AgendaRestFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
